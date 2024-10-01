e772eea9ebe316d5f201f11c16960c46a00e79a5
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-10-01 16:18:34 +0900

    Add Aqua, Test, and JET as test targets. (49)

---
+++ test/runtests.jl
@@ -0,0 +1,18 @@
+using Test
+
+using Aqua
+using JET
+
+using PlutoMonacoEditor
+
+@testset "Code quality (Aqua.jl)" begin
+    Aqua.test_all(PlutoMonacoEditor; deps_compat = false)
+end
+
+v = VERSION
+isreleased = v.prerelease == ()
+if isreleased && v >= v"1.10"
+    @testset "Code linting (JET.jl)" begin
+        JET.test_package(PlutoMonacoEditor; target_defined_modules = true)
+    end
+end
