0e5b5b0b31f40a256750608937ed7b05f2035918
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2025-01-19 14:49:31 +0900

    Update examples

---
+++ examples/main.cxx
@@ -0,0 +1,6 @@
+// Edit me
+#include<iostream>
+
+int main(){
+	std::cout<< "Hello" << std::endl;
+}
--- examples/python.jl
+++ examples/python.jl
@@ -1,94 +1,119 @@
 ### A Pluto.jl notebook ###
-# v0.19.46
+# v0.20.4
 
 using Markdown
 using InteractiveUtils
 
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
+    #! format: off
     quote
-        local iv = try
-            Base.loaded_modules[Base.PkgId(
-                Base.UUID("6e696c72-6542-2067-7265-42206c756150"),
-                "AbstractPlutoDingetjes",
-            )].Bonds.initial_value
-        catch
-            b -> missing
-        end
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
     end
+    #! format: on
 end
 
 # ╔═╡ 07ebdbbe-49bb-4d18-9f2b-14f98d137548
 begin
     using Pkg
     Pkg.activate(temp = true)
-    Pkg.add("PythonCall")
-    Pkg.add("CondaPkg")
-
-    # Load PlutoMonacoEditor package
+    Pkg.add(["PythonCall", "CondaPkg"])
     Pkg.develop(path = dirname(@__DIR__))
-
     using CondaPkg
+    CondaPkg.add("numpy")
     using PythonCall
-
     using PlutoMonacoEditor: MonacoEditor
 end
 
 # ╔═╡ 9aa40f8c-a955-47a3-8e6d-4ac54d1dc330
 md"""
 # Python editor on Pluto Notebook
+
+To run this notebook, you need to install `g++` command, a C++ compiler.
 """
 
-# ╔═╡ 3b4f5287-edfe-4d93-a0c1-f206f5da9ca7
-begin
-    # Install Python dependencies
-    CondaPkg.add_pip("numpy")
-end
-
-# ╔═╡ bf232d99-3001-4aac-afb7-1321c7407666
-begin
-    initCode = """
-    import numpy as np
-    def my_python_api():
-    	return np.random.random((3, 3))
-    """
-    @bind sourcecode MonacoEditor("python", initCode)
-end
-
-# ╔═╡ 31e9189a-de9d-472d-8f7d-9d2935fbffb4
+# ╔═╡ 7d458d1e-bc9f-4f3b-abb3-c8b817cd7d73
 md"""
-This binds Julia variable `pythoncode` and an instance of Monaco Editor. If one update the content of the Monaco Editor, the value of `pythoncode` is updated to the content. Pluto Notebook will re run cells related to `pythoncode`.
+The following code is executed behind the scenes.
+
+```julia
+begin
+    targetfile = joinpath(dirname(first(split(@__FILE__, ".jl#==#"))), "main.py")
+    initCode = join(readlines(targetfile), '\n')
+    @bind sourcecode MonacoEditor("python", initCode, height = 300)
+end
+```
 """
 
-# ╔═╡ 3af14431-6464-458e-a3d6-9a268b9f9349
+# ╔═╡ 4428e48e-d18c-4d8d-ae2e-39379fde6216
 begin
-    function loadpythonmodule(sourcecode)
-        write("mylib.py", sourcecode)
-        importlib = pyimport("importlib")
-        pyimport("sys")."path".append(@__DIR__)
-        mylib = pyimport("mylib")
-        importlib.reload(mylib)
-        mylib
+    targetfile = joinpath(dirname(first(split(@__FILE__, ".jl#==#"))), "main.py")
+    initCode = join(readlines(targetfile), '\n')
+    @bind sourcecode MonacoEditor("python", initCode, height = 300)
+end
+
+# ╔═╡ dce616d7-fdfe-4854-919d-420603ebc875
+let
+    shouldrun = false
+    if !ismissing(sourcecode)
+        if initCode != sourcecode
+            # Update file
+            write(targetfile, sourcecode)
+            # If updated, we should run code
+            shouldrun = true
+        end
+        if shouldrun
+            mktempdir() do d
+                write(joinpath(d, targetfile), sourcecode)
+                try
+                    python = PythonCall.C.CTX.exe_path
+                    run(`$python $(joinpath(d, targetfile))`)
+                catch e
+                    e isa ProcessFailedException || rethrow(e)
+                end
+            end
+        end
     end
-
-    mylib = loadpythonmodule(sourcecode)
-    nothing
 end
 
-# ╔═╡ 00014af8-c64b-4aad-b478-3b9ec36810da
-begin
-    np_arr = mylib.my_python_api()
-    @assert pyconvert(Bool, np_arr.shape == pytuple((3, 3)))
+# ╔═╡ 7b8519b8-9236-4564-93a8-41d08097291a
+md"""
+
+The following code is executed behind the scenes.
+
+```julia
+let
+    shouldrun = false
+    if !ismissing(sourcecode)
+        if initCode != sourcecode
+            # Update file
+            write(targetfile, sourcecode)
+            # If updated, we should run code
+            shouldrun = true
+        end
+        if shouldrun
+            mktempdir() do d
+                write(joinpath(d, targetfile), sourcecode)
+                try
+                    python = PythonCall.C.CTX.exe_path
+                    run(`$python $(joinpath(d, targetfile))`)
+                catch e
+                    e isa ProcessFailedException || rethrow(e)
+                end
+            end
+        end
+    end
 end
+```
+"""
 
 # ╔═╡ Cell order:
 # ╟─9aa40f8c-a955-47a3-8e6d-4ac54d1dc330
 # ╠═07ebdbbe-49bb-4d18-9f2b-14f98d137548
-# ╠═3b4f5287-edfe-4d93-a0c1-f206f5da9ca7
-# ╟─bf232d99-3001-4aac-afb7-1321c7407666
-# ╟─31e9189a-de9d-472d-8f7d-9d2935fbffb4
-# ╠═3af14431-6464-458e-a3d6-9a268b9f9349
-# ╠═00014af8-c64b-4aad-b478-3b9ec36810da
+# ╟─7d458d1e-bc9f-4f3b-abb3-c8b817cd7d73
+# ╟─4428e48e-d18c-4d8d-ae2e-39379fde6216
+# ╟─dce616d7-fdfe-4854-919d-420603ebc875
+# ╟─7b8519b8-9236-4564-93a8-41d08097291a
