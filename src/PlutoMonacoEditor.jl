539432641443dc4ffca9cd3d78df03a7d123074c
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2025-02-28 10:15:33 +0900

    Format

--- src/PlutoMonacoEditor.jl
+++ src/PlutoMonacoEditor.jl
@@ -14,31 +14,31 @@ function MonacoEditor(
     theme::AbstractString = "vs-dark",
 )
     @htl """
-   <span>
-
-   <script>
-   	const monaco = await import('https://cdn.jsdelivr.net/npm/monaco-editor@0.52.0/+esm');
-    const wrapper_span = currentScript.parentElement
-   	const monEditor = monaco.editor.create(wrapper_span.querySelector('div#monaco-editor-container'), {
-   		value: $(initCode),
-   		language: $(language),
-   		theme: $(theme)
-   	});
-
-   	function update_bond() {
-   		wrapper_span.value = monEditor.getValue();
-   		wrapper_span.dispatchEvent(new CustomEvent("update"));
-   	}
-   	
-   	const myEditor = wrapper_span.querySelector("#monaco-editor-container");
-   	myEditor.addEventListener("input", e=>{
-   		update_bond();
-   	})
-   	
-   	update_bond();
-   </script>
-   </span>
+    <span>
     <div id='monaco-editor-container' style="width: $(width)px; height: $(height)px; border: 1px solid #ddd;"></div>
+
+    <script>
+        const monaco = await import('https://cdn.jsdelivr.net/npm/monaco-editor@0.52.0/+esm');
+        const wrapper_span = currentScript.parentElement
+        const monEditor = monaco.editor.create(wrapper_span.querySelector('div#monaco-editor-container'), {
+            value: $(initCode),
+            language: $(language),
+            theme: $(theme)
+        });
+
+        function update_bond() {
+            wrapper_span.value = monEditor.getValue();
+            wrapper_span.dispatchEvent(new CustomEvent("update"));
+        }
+
+        const myEditor = wrapper_span.querySelector("#monaco-editor-container");
+        myEditor.addEventListener("input", e=>{
+            update_bond();
+        })
+
+        update_bond();
+    </script>
+    </span>
    """
 end
 
